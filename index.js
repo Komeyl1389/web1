72bf207cdaa91c5c425988f61503836d65957916
unknown <komeyleskandari1@gmail.com>
2024-05-09 08:42:34 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+import {PI2,mohit,masahat} from './math_utils.js'
+
+console.log(mohit(20))
+
+console.log(masahat(49))
+
+console.log('hello')
---
+++ math_utils.js
@@ -0,0 +1,9 @@
+export const PI2 = 3.14
+
+export function mohit(radius){
+return 2*radius*PI2
+
+}
+export function masahat(radius){
+    return radius*radius*PI2
+}
